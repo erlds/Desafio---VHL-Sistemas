932cb3d61cfa6b3474edd2ad1745027e5d1bab02
Evaristo Ramalho <evaristo.ra@hotmail.com>
2023-02-11 09:11:24 -0300

    Create Ente DTO

--- src/main/java/com/desafioVHL/api/DTO/Ente.java
+++ src/main/java/com/desafioVHL/api/DTO/Ente.java
@@ -1,7 +1,6 @@
 package com.desafioVHL.api.DTO;
 
 import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.Size;
 
 import lombok.Data;
 
@@ -9,19 +8,9 @@ import lombok.Data;
 public class Ente {
 
     @NotBlank
-    private String codigo;
+    private Long codigo;
     @NotBlank
     private String lei;
     @NotBlank
-    private String brandCar;
-    @NotBlank
-    private String modelCar;
-    @NotBlank
-    private String colorCar;
-    @NotBlank
-    private String responsibleName;
-    @NotBlank
-    private String apartment;
-    @NotBlank
-    private String block;
+    private String nomeDaEntidade;
 }
