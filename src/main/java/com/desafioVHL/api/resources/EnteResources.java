83184b7d8c7265ab1f5b6efa0f11a2a524623455
Evaristo Ramalho <evaristo.ra@hotmail.com>
2023-02-11 10:36:29 -0300

    Add EnteResource

---
+++ src/main/java/com/desafioVHL/api/resources/EnteResources.java
@@ -0,0 +1,25 @@
+package com.desafioVHL.api.resources;
+
+import com.desafioVHL.api.DTO.Ente;
+import com.desafioVHL.api.services.EnteService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@CrossOrigin(origins = "*",maxAge = 3600)
+@RequestMapping("/entes")
+public class EnteResources {
+
+    @Autowired
+    private EnteService service;
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public ResponseEntity<List<Ente>> getClienteById() {
+        return ResponseEntity.status(HttpStatus.OK).body(service.findAll());
+    }
+}
