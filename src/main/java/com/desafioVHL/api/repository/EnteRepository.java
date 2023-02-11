548030c9874bf9373d8cbdf84d2c92d5691dbdb4
Evaristo Ramalho <evaristo.ra@hotmail.com>
2023-02-11 12:10:20 -0300

    Create EnteRepository

---
+++ src/main/java/com/desafioVHL/api/repository/EnteRepository.java
@@ -0,0 +1,7 @@
+package com.desafioVHL.api.repository;
+
+import com.desafioVHL.api.entities.Ente;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EnteRepository extends JpaRepository<Ente, Integer> {
+}
