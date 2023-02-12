7e287ddac04dd320e0b859bb8bea1ec978a063ed
Evaristo Ramalho <evaristo.ra@hotmail.com>
2023-02-12 14:13:28 -0300

    Add SecurityConfig

---
+++ src/main/java/com/desafioVHL/api/config/SecurityConfig.java
@@ -0,0 +1,32 @@
+package com.desafioVHL.api.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http.cors().and().authorizeRequests()
+//                .antMatchers(SWAGGER_WHITELIST).permitAll()
+                .anyRequest().authenticated()
+                .and().httpBasic();
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers(
+                "/v2/api-docs",
+                "/configuration/ui",
+                "/swagger-resources/**",
+                "/configuration/security",
+                "/swagger-ui.html",
+                "/swagger-ui/**",
+                "/webjars/**");
+    }
+}
